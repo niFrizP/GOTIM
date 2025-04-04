3378321ab6125fd4fbbc6b32658a8980c210a79a
Иiкo FP <102751106+niFrizP@users.noreply.github.com>
2025-04-03 23:34:40 -0300

    Agregar traducciones al español y redirección automatica al login

---
+++ lang/es/passwords.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'password' => 'Las contraseñas deben tener al menos ocho caracteres y coincidir con la confirmación.',
+    'reset' => '¡Tu contraseña ha sido restablecida!',
+    'sent' => '¡Te hemos enviado por correo el enlace para restablecer tu contraseña!',
+    'token' => 'Este token de restablecimiento de contraseña es inválido.',
+    'user' => 'No podemos encontrar un usuario con esa dirección de correo electrónico.',
+    'throttled' => 'Por favor espera antes de intentar de nuevo.',
+    'passwords' => 'Las contraseñas no coinciden.',
+    'passwords_user' => 'La contraseña no coincide con la del usuario.',
+    'passwords_user_old' => 'La contraseña antigua no coincide con la del usuario.',
+    'passwords_user_new' => 'La nueva contraseña no puede ser igual a la anterior.',
+    'passwords_user_new_confirm' => 'La nueva contraseña no coincide con la confirmación.',
+];
