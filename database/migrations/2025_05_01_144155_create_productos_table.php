4a3e8985de382fd255ca8916a9fd7f2f02e5f897
Blanca12luz <bl.hidalgo@duocuc.cl>
2025-05-01 19:43:53 -0400

    Creacion de producto

---
+++ database/migrations/2025_05_01_144155_create_productos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_categoria')->constrained('categorias');
+            $table->string('marca')->nullable();
+            $table->string('modelo')->nullable();
+            $table->text('descripcion')->nullable();
+            $table->unsignedInteger('stock')->default(0);
+            $table->string('codigo')->unique(); // código único para seguimiento
+            $table->string('imagen')->nullable();
+            $table->boolean('activo')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
